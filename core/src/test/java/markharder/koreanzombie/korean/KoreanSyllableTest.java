b5e021e2f58250108b3c5e2eaa9819133061a76e
Mark Harder <mark.harder899@gmail.com>
2017-03-28 21:14:10 -0400

    Add tests and move source to main/
    Move the source code to src/main/ and tests to src/test/

---
+++ core/src/test/java/markharder/koreanzombie/korean/KoreanSyllableTest.java
@@ -0,0 +1,14 @@
+package markharder.koreanzombie.korean;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class KoreanSyllableTest {
+    @Test public void sample() {
+        assertEquals(true, true);
+    }
+
+    @Test public void sampleTwo() {
+        assertEquals(true, false);
+    }
+}
